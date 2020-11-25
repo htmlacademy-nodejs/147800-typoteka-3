b061980bc3c2866e59cbabccba522e45784a8683
Alexandr <alexandrsashin@gmail.com>
2020-11-26 02:20:34 +0300

    Added my articles on /my route

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,9 +1,15 @@
 "use strict";
 
 const { Router } = require(`express`);
+const axios = require(`axios`);
 const myRouter = new Router();
 
+const URL = `http://localhost:3000`;
+
 myRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
-myRouter.get(`/`, (req, res) => res.render(`my/my`));
+myRouter.get(`/`, async (req, res) => {
+  const { data } = await axios.get(`${URL}/api/articles`);
+  res.render(`my/my`, { articles: data });
+});
 
 module.exports = myRouter;
